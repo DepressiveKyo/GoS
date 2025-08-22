8d4693eb3e636810bbe41a7f52e1eac8591c73b9
DepressiveKyo <depressivekyosha@gmail.com>
2025-08-22 15:04:11 -0400

    Update currentVersion.lua

--- DepressiveAIONext/currentVersion.lua
+++ DepressiveAIONext/currentVersion.lua
@@ -11,9 +11,10 @@ Data = {
     Cassiopeia = { Version = 0.01, Changelog = "Cassiopeia" },
     Elise = { Version = 1.25, Changelog = "Elise" },
     Lillia = { Version = 0.01, Changelog = "Lillia" },
-    TwistedFate = { Version = 0.01, Changelog = "TwistedFate" },
+    TwistedFate = { Version = 0.02, Changelog = "TwistedFate" },
     }
 }
+
 
 
 
