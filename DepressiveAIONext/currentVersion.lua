76371cf94dc0f16ea73de2da642680bddad130f8
DepressiveKyo <depressivekyosha@gmail.com>
2025-11-28 17:02:59 -0300

    Update currentVersion.lua

--- DepressiveAIONext/currentVersion.lua
+++ DepressiveAIONext/currentVersion.lua
@@ -7,19 +7,22 @@ Data = {
         Version = 0.01,
     },
     Champions = {
-    Aatrox = { Version = 0.16, Changelog = "Aatrox" },
+    Aatrox = { Version = 2.0, Changelog = "Aatrox" },
     Ahri = { Version = 0.22, Changelog = "Ahri" },
     Akali = { Version = 0.47, Changelog = "Akali" },
     Aphelios = { Version = 1.0, Changelog = "Aphelios" },
     Cassiopeia = { Version = 0.01, Changelog = "Cassiopeia" },
     Elise = { Version = 1.25, Changelog = "Elise" },
-    Fiora = { Version = 1.0, Changelog = "Fiora" },
-    Lillia = { Version = 0.01, Changelog = "Lillia" },
+    Fiora = { Version = 2.1, Changelog = "Fiora" },
+    Katarina = { Version = 1.0, Changelog = "Katarina" },
+    Lillia = { Version = 1.0, Changelog = "Lillia" },
     Lulu = { Version = 1.0, Changelog = "Lulu" },
-    Mel = { Version = 1.0, Changelog = "Mel" },
-    TwistedFate = { Version = 0.02, Changelog = "TwistedFate" },
+    Mel = { Version = 0.1, Changelog = "Mel" },
     Riven = { Version = 1.0, Changelog = "Riven" },
+    TwistedFate = { Version = 1.8, Changelog = "TwistedFate" },
+    Vayne = { Version = 4.0, Changelog = "Vayne" },
     Yasuo = { Version = 3.5, Changelog = "Yasuo" },
     Zed = { Version = 1.0, Changelog = "Zed" },
     }
 }
+
